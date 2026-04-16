6f322c0525dff4f3ac872bca24bd5fadb6c9f257
Intissar Soulaiman <intisarsulaimann@gmail.com>
2026-04-16 16:10:48 +0300

    Add booking routes

--- backend/routes/bookings.js
+++ backend/routes/bookings.js
@@ -5,7 +5,7 @@ const authMiddleware = require('../middleware/auth');
 const bookingController = require('../controllers/bookingController');
 
 router.use(authMiddleware);
-
+// Booking routes
 router.post('/', bookingController.createBooking);
 router.get('/my', bookingController.getMyBookings);
 router.get('/:id', bookingController.getOneBooking);
