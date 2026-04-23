4d0080574e93317026b49047e48c223b57a93ab1
Lea Ouslati <ouslatilea@gmail.com>
2026-04-23 21:08:05 +0300

    prepare for deployment

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,12 +1,9 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_NAME:', process.env.DB_NAME);
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
@@ -14,6 +11,7 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0,
   dateStrings: true,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
