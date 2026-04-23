b2f194aba9b80c05287d7800551dde9620622e8c
Lea Ouslati <ouslatilea@gmail.com>
2026-04-23 21:33:48 +0300

    Added the backend url

--- frontend/api/axios.js
+++ frontend/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://polaristech-travelagencymanagementsystem.onrender.com'
 });
 
 api.interceptors.request.use((config) => {
